0d4c8520298493afc8a819537e440abfda1e1f0a
Alejandro Rello <alejandrorello.dev@gmail.com>
2025-04-24 18:35:06 +0000

    Agragados links a navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,10 +4,25 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<img width={'80px'} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" alt="star wars logo"/>
-			<button>
-				Favorites
-			</button>
+			<Link to="/">
+				<img width={'80px'} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/694px-Star_Wars_Logo.svg.png" alt="Star Wars logo" />
+			</Link>
+			<ul className="nav nav-pills">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">Characters</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/Ships" className="nav-link">Ships</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/Planets" className="nav-link">Planets</Link>
+                </li>
+            </ul>
+			<div className="ml-auto">
+				
+					<button className="btn btn-primary">Favoritos</button>
+				
+			</div>
 		</nav>
 	);
 };
